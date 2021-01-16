f64bc8cbbb4e9b3134d1854a57a2f74de6695013
Abirami.R <77112799+Abi-05@users.noreply.github.com>
2021-01-16 10:16:23 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,4 +1,4 @@
-#radius of the circle
+#python program which accepts the radius of a circle 
 radius=float(input("Input the circle of radius :"))
-area=3.14*radius*radius
-print("The radius of the circle with radius",radius,"is:",area)
+area=((22/7)*radius*radius)
+print("The area of the circle with radius",radius,"is:",area)
